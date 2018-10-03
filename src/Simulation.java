c93eb6caaeb1aa46814fa84daafcdce6fb8383f0
Nyasha <nyashabryan23@gmail.com>
2018-10-03 16:44:33 +0100

    Added the simulation loop using trees 2D array

--- src/Simulation.java
+++ src/Simulation.java
@@ -2,19 +2,41 @@ import java.lang.Thread;
 
 public class Simulation extends Thread{
 
-    Tree[] trees;
+    Tree[][] trees;
+    Land sunmap;
 
-    public Simulation(Tree[] trees){
+    public Simulation(Tree[][] trees, Land sunmap){
         this.trees = trees;        
+        this.sunmap = sunmap;
     }
 
     @Override
     public void run(){
-        try{
-            Thread.sleep(2000);    
-        }catch (InterruptedException e){
-            e.printStackTrace();
+
+        while(true){    
+            try{
+                for(int i = 9; i >= 0; i--){
+                    for (Tree tree: trees[i]){
+                        tree.simulate(sunmap);
+                    }
+                }
+                sunmap.resetSunlight();
+                try{
+                    Thread.sleep(2000);
+                }
+                catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+            
+            }catch(ArrayIndexOutOfBoundsException e){
+                e.printStackTrace();
+                for(int i = 0; i < trees.length; i++)
+                for (Tree tree: trees[i]){
+                    tree.setExt(0.6f);
+                }
+                // Reset the grid
+                sunmap.resetSunlight();
+            }
         }
-        
     }
 }
