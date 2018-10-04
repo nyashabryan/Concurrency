6a1fd9f977bc2fefca9e9be8a8ffb46f74e7775c
Nyasha <nyashabryan23@gmail.com>
2018-10-04 12:37:44 +0100

    Added changes to the Forest Panel class

--- src/ForestPanel.java
+++ src/ForestPanel.java
@@ -6,7 +6,9 @@ import javax.swing.JPanel;
 import java.util.*;
 import java.util.concurrent.ForkJoinPool;
 
-
+/**
+ * Custom JPanel class to represent the Forest rendering in the GUI.
+ */
 public class ForestPanel extends JPanel implements Runnable {
 	Tree[][] forest;	// trees to render
 	List<Integer> rndorder; // permutation of tree indices so that rendering is less structured
@@ -15,24 +17,28 @@ public class ForestPanel extends JPanel implements Runnable {
 		forest=trees;
 	}
 
+	/**
+	 * Overriden paintComponent method of the JPanel object inhereted by the 
+	 * Forest panel object. Handles how the ForestPanel object is rendered.
+	 */
+	@Override
 	public void paintComponent(Graphics g) {
 		int width = getWidth();
 		int height = getHeight();
 		g.clearRect(0,0,width,height);
 		    
-		// draw the forest in different canopy passes
-		Random rnd = new Random(0); // providing the same seed gives trees consistent colouring
 		long init = System.currentTimeMillis();
-
-
 		// Use a divide and conquer algorithm to solve this problem
 		for(int i =0; i < forest.length; i++){
 			pool.invoke(new RenderParallel(g, forest[i], 0, forest[i].length, width, height));
 		}
-		
-		//System.out.println(System.currentTimeMillis() - init);
+		System.out.println(System.currentTimeMillis() - init);
 	}
 	
+	/**
+	 * Implementation of the run method of the Forest Panel class. Overidden from the Runnable interface it 
+	 * implements. 
+	 */
 	public void run() {
 			
 		// reordering so that trees are rendered in a more random fashion
@@ -52,6 +58,10 @@ public class ForestPanel extends JPanel implements Runnable {
 		}
 	}
 
+	/**
+	 * Recursive action class responsible for rendering the trees in the 
+	 * GUI.
+	 */
 	class RenderParallel extends RecursiveAction{
 
 		Graphics g;
@@ -61,6 +71,17 @@ public class ForestPanel extends JPanel implements Runnable {
 		int height;
 		int width;
 
+		/**
+		 * Default Constructor for the RenderParallel Class
+		 * @param g The Graphics object which is being rendered on.
+		 * @param trees The array of trees in the extent class being rendered. 
+		 * @param low The boundary of the section of the trees array the object is responsible
+		 * 			for.
+		 * @param high The boundary of the section of the trees array the object is responsible
+		 * 			for.
+		 * @param width The width of the Forest Panel object.
+		 * @param height The height of the Forest Panel object.
+		 */
 		RenderParallel(Graphics g, Tree[] trees, int low, int high, int width, int height){
 			this.g = g;
 			this.trees = trees;
@@ -70,30 +91,29 @@ public class ForestPanel extends JPanel implements Runnable {
 			this.height = height;
 		}
 
+		/**
+		 * Overriden compute method of the RenderParallel class from the Recursive Action class.
+		 * Handles the computation of the RenderParallel object.
+		 */
 		@Override
-		public void compute(){
-			if(high -low > 20000){
+		protected void compute(){
+			if(high -low > (int)trees.length){
 				RenderParallel left = new RenderParallel(g, trees, low, (int)high/2, width, height);
 				RenderParallel right = new RenderParallel(g, trees, (int)high/2, high, width, height);
-
 				right.fork();
 				left.compute();
 				right.join();
 
 			}else{
-				// start from small trees of [0, 2) extent
-				float minh = 0.0f;
-				float maxh = 2.0f;
-
-				for(int layer = 0; layer <= 10; layer++) {
 					for(int rt = low; rt < high; rt++){
-						if(trees[rt].getExtent() >= minh && trees[rt].getExtent() < maxh) { // only render trees in current band
-							// draw trees as rectangles centered on getX, getY with random greenish colour
 							g.setColor(trees[rt].color);
-							int y1 = trees[rt].getY() - (int) trees[rt].getExtent();
-							int x1 = trees[rt].getX() - (int) trees[rt].getExtent();
-							int x2 = trees[rt].getX() + (int) trees[rt].getExtent();
-							int y2 = trees[rt].getY() + (int) trees[rt].getExtent();
+					int X = trees[rt].getX();
+					int Y = trees[rt].getY();
+					int extent = (int)trees[rt].getExtent();
+					int y1 = Y - extent;
+					int x1 = X - extent;
+					int x2 = X + extent;
+					int y2 = Y + extent;
 							if (y1 < 0)
 								y1 = 0;
 							if (x1 < 0)
@@ -104,12 +124,6 @@ public class ForestPanel extends JPanel implements Runnable {
 								y2 = height;
 							g.fillRect(x1, y1, x2 - x1, y2 -y1);
 						}
-						// g.setColor(Color.black);
-						// g.fillRect(forest[rt].getY(), forest[rt].getX(), 1, 1); // draw the trunk
-					}
-					minh = maxh;  // next band of trees
-					maxh += 2.0f;
-				}
 			}
 		}
 	}
