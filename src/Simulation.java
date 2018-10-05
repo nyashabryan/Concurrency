b676c35f1f89a78e6b53792c7bdc8cf28965fcda
Nyasha Bryan <nyashabryan23@gmail.com>
2018-10-05 21:01:08 +0200

    Ported to Windows and Added SimRunner class

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,4 +1,6 @@
 import java.lang.Thread;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Simulation extends Thread{
@@ -6,6 +8,7 @@ public class Simulation extends Thread{
     Tree[][] trees;
     Land sunmap;
     AtomicBoolean running;
+    ForkJoinPool pool = new ForkJoinPool();
 
     public Simulation(Tree[][] trees, Land sunmap, AtomicBoolean running){
         this.trees = trees;        
@@ -20,9 +23,7 @@ public class Simulation extends Thread{
             try{
                 while(!running.get()){}
                 for(int i = 9; i >= 0; i--){
-                    for (Tree tree: trees[i]){
-                        tree.simulate(sunmap);
-                    }
+                    pool.invoke(new SimRunner(trees[i], 0, trees[i].length, sunmap));
                 }
                 sunmap.yearNumber.incrementAndGet();
                 sunmap.resetSunlight();
@@ -43,5 +44,36 @@ public class Simulation extends Thread{
                 sunmap.resetSunlight();
             }
         }
+    }
+
+    public class SimRunner extends RecursiveAction{
+
+        Tree[] trees;
+        int low;
+        int high;
+        Land sunmap;
+
+        public SimRunner(Tree[] trees, int low, int high, Land sunmap){
+            this.trees = trees;
+            this.low = low;
+            this.high = high;
+            this.sunmap = sunmap;
+        }
+
+        @Override
+        protected void compute(){
+            if(this.high - this.low < (int)(this.trees.length/2)){
+                for(int i = low; i < high; i++){
+                    trees[i].simulate(sunmap);
+                }
+            }else{
+                SimRunner left = new SimRunner(trees, this.low, (int)((this.low + this.high)/2), sunmap);
+                SimRunner right = new SimRunner(trees, (int)((this.low + this.high)/2), this.high, sunmap);
+                right.fork();
+                left.compute();
+                right.join();
+            }
+        }
+
     }
 }
